a180e97a22c889e2f2c772074b53e5248d20a3e9
Elias <Reiter.Elias@sus.htl-grieskirchen.at>
2020-09-24 16:47:03 +0200

    Main method Test

--- FirstProject/src/Main.java
+++ FirstProject/src/Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
         EratosthenesPrimeSieve e = new EratosthenesPrimeSieve(20);
         e.printPrimes();
-
+        e.primeAlgorithm();
     }
 }
